38b0acd0b932ff0512716df6704ead08324a3bec
AnaBrh <anamariebrh@gmail.com>
2023-11-24 09:34:14 +0000

    separated comment count test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -140,19 +140,38 @@ describe("GET /api/articles", () => {
 });
 
 describe("GET /api/articles/:article_id", () => {
-	test("200: accepts a article_id query which responds with all articles with that id, including comment count", () => {
+	test("200: accepts a article_id query which responds with all articles with that id", () => {
 		return request(app)
 			.get("/api/articles/1")
 			.expect(200)
 			.then(({ body }) => {
 				const { article } = body;
-                    expect(article).toHaveProperty("comment_count", '11')
 					expect(article).toMatchObject({
 						article_id: 1,
 						title: "Living in the shadow of a great man",
 						topic: "mitch",
 						author: "butter_bridge",
 						body: "I find this existence challenging",
+						created_at: "2020-07-09T20:11:00.000Z",
+						votes: 100,
+						article_img_url:
+							"https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+					});
+			});
+	});
+	test('200: returns all articles with the matching id, as well as the total comment count', () => {
+		return request(app)
+			.get("/api/articles/1")
+			.expect(200)
+			.then(({ body }) => {
+				const { article } = body;
+					expect(article).toMatchObject({
+						article_id: 1,
+						title: "Living in the shadow of a great man",
+						topic: "mitch",
+						author: "butter_bridge",
+						body: "I find this existence challenging",
+						comment_count: '11',
 						created_at: "2020-07-09T20:11:00.000Z",
 						votes: 100,
 						article_img_url:
